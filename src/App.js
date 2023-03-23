39713d74f47738738e49295287724b48893ad493
Your Name <jlmeeks587@gmail.com>
2023-03-23 19:36:25 -0400

    Available title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
 import { Card } from "./components/Card";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <h1 className="title">available</h1>
       <div className="card-container">
         <Card />
       </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Card.css';
 
 export const Card = () => {
   return (
     <div className='card'>
-      Card
+      <img src="/IMG_0405.jpg" alt="name" />
+      <div className="description">
+        age weight sex<br/>
+        price<br/>
+        <button>Add to cart</button>
+      </div>
     </div>
   )
 }
