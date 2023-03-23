720cf9002e84246761b36e54aed0570541d12163
Your Name <jlmeeks587@gmail.com>
2023-03-23 15:55:26 -0400

    :

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
+import { Card } from "./components/Card";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <div className="card-container">
+        <Card />
+      </div>
     </div>
   );
 }
