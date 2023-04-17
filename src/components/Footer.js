75d6c9656048638a9480b9a0e85b86be1fc97f3a
Your Name <jlmeeks587@gmail.com>
2023-04-17 10:53:58 -0400

    Testing file issues with VS Code and Github

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+export const Footer = () => {
+  return (
+    <footer>
+      <span>&copy; Southern Genetics test</span>
+    </footer>
+  )
+}
---
+++ src/components/ImageCarousel.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ImageCarousel.css';
+
+export const ImageCarousel = ({ available }) => {
+  const {img} = available[0]
+  return (
+    <div className='carousel-container'>
+      <img className='selected-img' src={img} alt="gecko image" />
+      <div className="unselected-img-container">
+        <img className='unselected-img' src={img} alt="gecko" />
+        <img className='unselected-img' src={img} alt="gecko image" />
+        <img className='unselected-img' src={img} alt="gecko image" />
+        <img className='unselected-img' src={img} alt="gecko image" />
+      </div>
+    </div>
+  )
+}
