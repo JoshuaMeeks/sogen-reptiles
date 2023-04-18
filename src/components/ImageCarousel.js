4abf05356989ae9ff239d04081efe29fc9837494
Your Name <jlmeeks587@gmail.com>
2023-04-18 18:03:04 -0400

    Image carousel selection functional, using useEffect

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './ImageCarousel.css';
 
 export const ImageCarousel = ({ available }) => {
@@ -6,33 +6,19 @@ export const ImageCarousel = ({ available }) => {
   const [selected, setSelected] = useState(imgs[0])
   const [unselected, setUnselected] = useState(imgs.filter(img => img !== selected))
 
-  //future notes: clicking selected image will enlarge it
-
-  //make first image the selected image by default
-  //
-  
-  //allow for only one selected image at a time
-  
-  
-  //map through remaining images and display them as selectable thumbnails 
-  
-  
-  //clicking a thumbnail results in image being displayed as selected
+  useEffect(() => {
+    setUnselected(imgs.filter(img => img !== selected))
+  }, selected)
 
   return (
     <div className='carousel-container'>
-      {console.log(unselected)}
       <img className='selected-img' src={selected} alt="gecko" />
       <div className="unselected-img-container">
         {unselected.map(img => {
           return (
-            <img className='unselected-img' src={img} alt='gecko' />
+            <img onClick={() => {setSelected(img)}} className='unselected-img' src={img} alt='gecko' />
           )
         })}
-        {/* <img className='unselected-img' src={imgs} alt="gecko" />
-        <img className='unselected-img' src={imgs} alt="gecko" />
-        <img className='unselected-img' src={imgs} alt="gecko" />
-        <img className='unselected-img' src={imgs} alt="gecko" /> */}
       </div>
     </div>
   )
