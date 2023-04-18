99fb6ac266d1185ed58e0cd7b16189b99bd525f5
Your Name <jlmeeks587@gmail.com>
2023-04-18 17:26:34 -0400

    Add filter method and map method to image carousel component for a selectable image carousel

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -3,7 +3,8 @@ import './ImageCarousel.css';
 
 export const ImageCarousel = ({ available }) => {
   const {imgs} = available[0]  
-  const [selected, setSelected] = useState(false)
+  const [selected, setSelected] = useState(imgs[0])
+  const [unselected, setUnselected] = useState(imgs.filter(img => img !== selected))
 
   //future notes: clicking selected image will enlarge it
 
@@ -20,12 +21,18 @@ export const ImageCarousel = ({ available }) => {
 
   return (
     <div className='carousel-container'>
-      <img className='selected-img' src={imgs[0]} alt="gecko" />
+      {console.log(unselected)}
+      <img className='selected-img' src={selected} alt="gecko" />
       <div className="unselected-img-container">
+        {unselected.map(img => {
+          return (
+            <img className='unselected-img' src={img} alt='gecko' />
+          )
+        })}
+        {/* <img className='unselected-img' src={imgs} alt="gecko" />
         <img className='unselected-img' src={imgs} alt="gecko" />
         <img className='unselected-img' src={imgs} alt="gecko" />
-        <img className='unselected-img' src={imgs} alt="gecko" />
-        <img className='unselected-img' src={imgs} alt="gecko" />
+        <img className='unselected-img' src={imgs} alt="gecko" /> */}
       </div>
     </div>
   )
