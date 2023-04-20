0a3c497a512cd03e23799cca4389fa3bd4ac7ae9
Your Name <jlmeeks587@gmail.com>
2023-04-20 11:22:02 -0400

    Clean up ImgModal component

--- src/components/ImgModal.js
+++ src/components/ImgModal.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import { ImageCarousel } from './ImageCarousel';
+import React, { useState, useEffect } from 'react';
 import './ImgModal.css';
 
-export const ImgModal = ({ modal, available, toggleModal }) => {
+export const ImgModal = ({ available, modal, toggleModal }) => {
+  const {imgs} = available[0]  
+  const [selected, setSelected] = useState(imgs[0])
+  const [unselected, setUnselected] = useState(imgs.filter(img => img !== selected))
+
+  useEffect(() => {
+    setUnselected(imgs.filter(img => img !== selected))
+  }, selected)
+
   if (modal) {
     return (
-      <div className="modal-container" onClick={() => toggleModal()}>
-        <ImageCarousel 
-          available={available}
-        />
+      <div className='modal-background' onClick={() => toggleModal()}>
+        <div className='modal-container' onClick={e => e.stopPropagation()}>
+          <img className='modal-selected-img' src={selected} alt="gecko" />
+          <div className="unselected-img-container">
+            {unselected.map(img => {
+              return (
+                <img onClick={() => {setSelected(img)}} className='modal-unselected-img' src={img} alt='gecko' />
+              )
+            })}
+          </div>
+        </div>
       </div>
     )
   }
