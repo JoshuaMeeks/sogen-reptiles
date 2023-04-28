d29050683aecc7f6eefee2f89a4764c2c3b67f63
Your Name <jlmeeks587@gmail.com>
2023-04-28 00:00:53 -0400

    Add logic to next/previous image arrows in image modal

--- src/components/ImgModal.js
+++ src/components/ImgModal.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ImgModal.css';
 import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 
-export const ImgModal = ({ modal, selected, setSelected, unselected, toggleModal }) => {
+export const ImgModal = ({ modal, selected, unselected, toggleModal }) => {
+  const [modalSelected, setModalSelected] = useState(selected);
+  const completeImageList = [selected, ...unselected];
+  let currentIndex = completeImageList.indexOf(modalSelected)
+
+  const nextImage = () => {
+    if (currentIndex + 1 >= completeImageList.length) {
+      setModalSelected(completeImageList[0]);
+    } else {
+      setModalSelected(completeImageList[currentIndex + 1]);
+    }
+  }
+
+  const previousImage = () => {
+    if (currentIndex - 1 < 0) {
+      setModalSelected(completeImageList[completeImageList.length - 1]);
+    } else {
+      setModalSelected(completeImageList[currentIndex - 1]);
+    }
+  }
 
   if (modal) {
     document.querySelector('body').style.overflow = "hidden";
     document.documentElement.scrollTop = 0;
-    document.body.scrollTop = 0;
+    document.body.scrollTop = 0; 
+
     return (
       <div className='modal-background' onClick={() => toggleModal()}>
         <div className='modal-container' onClick={e => e.stopPropagation()}>
           <button className='close-btn' onClick={toggleModal}>&times;</button>
           <div>
-            <BsChevronLeft className='chevron' />
-            <img className='modal-selected-img' src={selected} alt="gecko" />
-            <BsChevronRight className='chevron' />
+            <BsChevronLeft className='chevron' onClick={() => previousImage()} />
+            <img className='modal-selected-img' src={modalSelected} alt="gecko" />
+            <BsChevronRight className='chevron' onClick={() => nextImage()} />
           </div>
-          {/* <div className="unselected-img-container">
-            {unselected.map(img => {
-              return (
-                <img onClick={() => {setSelected(img)}} className='modal-unselected-img' src={img} alt='gecko' />
-              )
-            })}
-          </div> */}
         </div>
       </div>
     )
