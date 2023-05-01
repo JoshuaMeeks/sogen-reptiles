a126192cc4af62d033f178d60c1138303351c13a
Your Name <jlmeeks587@gmail.com>
2023-05-01 13:07:55 -0400

    Add contact form to Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Contact = () => {
+  const [status, setStatus] = useState("Submit");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
+  };
+
   return (
     <div className="title-container">
       <h1 className="title">contact us</h1>
       <p>You can contact us via email at <a href="#">contact@sogenreptiles.com</a> or visit our Instagram page at <a href="#">SoGen Reptiles</a></p>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" required />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" required />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" required />
+        </div>
+        <button type="submit">{status}</button>
+      </form>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,8 +17,8 @@ export const Navbar = () => {
         <ul className='links'>
           <Link to="/about"><li>about</li></Link>
           <Link to="/available"><li>available</li></Link>
-          <Link to="/contact"><li>contact</li></Link>
           <Link to="/termsconditions"><li>terms and conditions</li></Link>
+          <Link to="/contact"><li>contact</li></Link>
         </ul>
       </nav>
     </header>
