72cc242f65179e99ec3ed1917e0245b8c47ad90c
Your Name <jlmeeks587@gmail.com>
2023-05-09 21:07:58 -0400

    Logic behind total amount in Cart component using reduce method and converting strings into numbers

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Cart.css';
 
 export const Cart = ({ cart }) => {
+  let total = cart.map(item => item.price);
   return (
     <div className="title-container">
       <h1 className="title">cart</h1>
@@ -13,6 +14,14 @@ export const Cart = ({ cart }) => {
             </div>
           )
         })}
+        <div className="item-container">
+          <p>Flatrate shipping</p>
+          <p className="cart-price">50</p>
+        </div>
+        <div className="item-container">
+          <p>Total</p>
+          <p className="cart-price">{total.map(Number).reduce((acc, curr) => acc + curr, 0) + 50}</p>
+        </div>
     </div>
   )
 }
